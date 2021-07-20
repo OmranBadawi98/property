67f85761f2a85cf45e024203d369a8c5101698f4
Omran <omranbadawi98@gmail.com>
2021-07-20 20:40:50 +0300

    edit home2

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { AiFillInstagram } from 'react-icons/ai'
+import { AiFillInstagram, AiOutlineWhatsApp } from 'react-icons/ai'
 import {
   FaFacebookF,
   FaLinkedinIn,
   FaSnapchatGhost,
   FaTwitter,
   FaYoutube,
+  FaSearch,
+  FaArrowUp,
+  FaCircle,
 } from 'react-icons/fa'
 import Homes from '../../images/homes.jpeg'
 
@@ -14,9 +17,8 @@ function Home() {
     <>
       <div className='absolute w-full h-auto bg-yellow-300'>
         <img src={Homes} alt='home' className='w-full relative ' />
-
-        <div className='absolute bottom-0 pr-12 pb-8 '>
-          <div className='pr-12 ml-auto h-full space-x-5 space-x-reverse flex'>
+        <div className='absolute bottom-0 pr-12 pb-7'>
+          <div className='ml-auto h-full space-x-5 space-x-reverse flex'>
             <div className='flex items-center'>
               <button className='border-2 bg-gray-300 rounded-full h-12 w-12'>
                 <FaSnapchatGhost className='w-7 h-7 text-red-600 m-auto' />
@@ -43,10 +45,23 @@ function Home() {
               <div className='pr-2 text-xl'>النشرة البريدية</div>
             </div>
           </div>
-          <div>
-            <button className='mr-auto'>button1</button>
-            <button className='mr-auto'>button1</button>
-          </div>
+        </div>
+        <div className='items-center mr-auto absolute bottom-0 left-0 pl-12'>
+          <button className='ml-4 h-16 bg-blue-500 w-36'>
+            <FaSearch />
+            أبحث
+            <FaArrowUp />
+          </button>
+          <button className='mr-4 h-16 bg-blue-500 w-36'>
+            <FaSearch />
+            بحث متقدم
+            <FaArrowUp />
+          </button>
+        </div>
+        <div className='w-60 h-20 '>
+          <AiOutlineWhatsApp>
+            <FaCircle />
+          </AiOutlineWhatsApp>
         </div>
       </div>
       {/* <div className=''>
